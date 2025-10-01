9ab2a2c0de6a3cbcddd87766da631265d9449300
SenpaiXN <chikushinemathew@gmail.com>
2025-10-01 20:30:18 +0000

    a04e: initial vendor tree

---
+++ a04e-vendor.mk
@@ -0,0 +1,39 @@
+#
+# Automatically generated file. DO NOT MODIFY
+#
+
+PRODUCT_SOONG_NAMESPACES += \
+    vendor/samsung/a04e
+
+PRODUCT_COPY_FILES += \
+    vendor/samsung/a04e/proprietary/vendor/tee/00000000-0000-0000-0000-000000534b4d:$(TARGET_COPY_OUT_VENDOR)/tee/00000000-0000-0000-0000-000000534b4d \
+    vendor/samsung/a04e/proprietary/vendor/tee/00000000-0000-0000-0000-0000534b504d:$(TARGET_COPY_OUT_VENDOR)/tee/00000000-0000-0000-0000-0000534b504d \
+    vendor/samsung/a04e/proprietary/vendor/tee/00000000-0000-0000-0000-474154454b45:$(TARGET_COPY_OUT_VENDOR)/tee/00000000-0000-0000-0000-474154454b45 \
+    vendor/samsung/a04e/proprietary/vendor/tee/00000000-0000-0000-0000-4b45594d5354:$(TARGET_COPY_OUT_VENDOR)/tee/00000000-0000-0000-0000-4b45594d5354 \
+    vendor/samsung/a04e/proprietary/vendor/tee/00000000-0000-0000-0000-505256544545:$(TARGET_COPY_OUT_VENDOR)/tee/00000000-0000-0000-0000-505256544545 \
+    vendor/samsung/a04e/proprietary/vendor/tee/00000000-0000-0000-0000-564c544b5052:$(TARGET_COPY_OUT_VENDOR)/tee/00000000-0000-0000-0000-564c544b5052 \
+    vendor/samsung/a04e/proprietary/vendor/tee/00000000-0000-0000-0000-6b6e78677564:$(TARGET_COPY_OUT_VENDOR)/tee/00000000-0000-0000-0000-6b6e78677564 \
+    vendor/samsung/a04e/proprietary/vendor/tee/00000000-4875-4171-496e-466163656871:$(TARGET_COPY_OUT_VENDOR)/tee/00000000-4875-4171-496e-466163656871 \
+    vendor/samsung/a04e/proprietary/vendor/tee/00000000-4875-4171-496e-46704348706e:$(TARGET_COPY_OUT_VENDOR)/tee/00000000-4875-4171-496e-46704348706e \
+    vendor/samsung/a04e/proprietary/vendor/tee/00000000-4875-4171-496e-467043646650:$(TARGET_COPY_OUT_VENDOR)/tee/00000000-4875-4171-496e-467043646650 \
+    vendor/samsung/a04e/proprietary/vendor/tee/00000000-4875-4171-496e-467045677354:$(TARGET_COPY_OUT_VENDOR)/tee/00000000-4875-4171-496e-467045677354 \
+    vendor/samsung/a04e/proprietary/vendor/tee/00000000-4875-4171-496e-467053696c64:$(TARGET_COPY_OUT_VENDOR)/tee/00000000-4875-4171-496e-467053696c64 \
+    vendor/samsung/a04e/proprietary/vendor/tee/00000000-4d54-4b5f-4246-0000004d3455:$(TARGET_COPY_OUT_VENDOR)/tee/00000000-4d54-4b5f-4246-0000004d3455 \
+    vendor/samsung/a04e/proprietary/vendor/tee/00000000-4d54-4b5f-4246-434d44515441:$(TARGET_COPY_OUT_VENDOR)/tee/00000000-4d54-4b5f-4246-434d44515441 \
+    vendor/samsung/a04e/proprietary/vendor/tee/00000000-4d54-4b5f-4246-444150437461:$(TARGET_COPY_OUT_VENDOR)/tee/00000000-4d54-4b5f-4246-444150437461 \
+    vendor/samsung/a04e/proprietary/vendor/tee/00000000-4d54-4b5f-4246-44524d4b5052:$(TARGET_COPY_OUT_VENDOR)/tee/00000000-4d54-4b5f-4246-44524d4b5052 \
+    vendor/samsung/a04e/proprietary/vendor/tee/00000000-4d54-4b5f-4246-534d454d5441:$(TARGET_COPY_OUT_VENDOR)/tee/00000000-4d54-4b5f-4246-534d454d5441 \
+    vendor/samsung/a04e/proprietary/vendor/tee/00000000-4d54-4b5f-4246-564443323634:$(TARGET_COPY_OUT_VENDOR)/tee/00000000-4d54-4b5f-4246-564443323634 \
+    vendor/samsung/a04e/proprietary/vendor/tee/00000000-4d54-4b5f-4246-57564e544100:$(TARGET_COPY_OUT_VENDOR)/tee/00000000-4d54-4b5f-4246-57564e544100 \
+    vendor/samsung/a04e/proprietary/vendor/tee/00000000-4d54-4b5f-4246-72706d627461:$(TARGET_COPY_OUT_VENDOR)/tee/00000000-4d54-4b5f-4246-72706d627461 \
+    vendor/samsung/a04e/proprietary/vendor/tee/driver/00000000-0000-0000-0000-564c544b4456:$(TARGET_COPY_OUT_VENDOR)/tee/driver/00000000-0000-0000-0000-564c544b4456 \
+    vendor/samsung/a04e/proprietary/vendor/tee/driver/00000000-4d54-4b5f-4246-434d44445256:$(TARGET_COPY_OUT_VENDOR)/tee/driver/00000000-4d54-4b5f-4246-434d44445256 \
+    vendor/samsung/a04e/proprietary/vendor/tee/driver/00000000-4d54-4b5f-4246-444150436472:$(TARGET_COPY_OUT_VENDOR)/tee/driver/00000000-4d54-4b5f-4246-444150436472 \
+    vendor/samsung/a04e/proprietary/vendor/tee/driver/00000000-4d54-4b5f-4246-445248444350:$(TARGET_COPY_OUT_VENDOR)/tee/driver/00000000-4d54-4b5f-4246-445248444350 \
+    vendor/samsung/a04e/proprietary/vendor/tee/driver/00000000-4d54-4b5f-4246-44524d4b5044:$(TARGET_COPY_OUT_VENDOR)/tee/driver/00000000-4d54-4b5f-4246-44524d4b5044 \
+    vendor/samsung/a04e/proprietary/vendor/tee/driver/00000000-4d54-4b5f-4246-4d3455445441:$(TARGET_COPY_OUT_VENDOR)/tee/driver/00000000-4d54-4b5f-4246-4d3455445441 \
+    vendor/samsung/a04e/proprietary/vendor/tee/driver/00000000-4d54-4b5f-4246-534d45647276:$(TARGET_COPY_OUT_VENDOR)/tee/driver/00000000-4d54-4b5f-4246-534d45647276 \
+    vendor/samsung/a04e/proprietary/vendor/tee/driver/00000000-4d54-4b5f-4246-564452323634:$(TARGET_COPY_OUT_VENDOR)/tee/driver/00000000-4d54-4b5f-4246-564452323634 \
+    vendor/samsung/a04e/proprietary/vendor/tee/driver/00000000-4d54-4b5f-4246-57564e647276:$(TARGET_COPY_OUT_VENDOR)/tee/driver/00000000-4d54-4b5f-4246-57564e647276 \
+    vendor/samsung/a04e/proprietary/vendor/tee/driver/18d9f073-18a5-4ade-9def-875e07f7f293_:$(TARGET_COPY_OUT_VENDOR)/tee/driver/18d9f073-18a5-4ade-9def-875e07f7f293_ \
+    vendor/samsung/a04e/proprietary/vendor/tee/startup.tzar:$(TARGET_COPY_OUT_VENDOR)/tee/startup.tzar
